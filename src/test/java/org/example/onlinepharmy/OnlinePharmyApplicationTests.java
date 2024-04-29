4b5a21707812ca423d0f47b697a00c812c1de66d
Macshop <macshop@Macshops-MacBook-Pro.local>
2024-04-29 16:50:53 +0500

    first commit

---
+++ src/test/java/org/example/onlinepharmy/OnlinePharmyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinepharmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePharmyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
