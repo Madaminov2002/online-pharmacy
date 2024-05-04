6fc33b398fce101fdb8fb12ee8b262f85a8f8880
Quvonchbek <fendygamer2006@gmail.com>
2024-05-04 12:21:20 +0500

    Added Repository

--- src/main/java/org/example/onlinepharmy/repo/UserRepository.java
+++ src/main/java/org/example/onlinepharmy/repo/UserRepository.java
@@ -2,7 +2,9 @@ package org.example.onlinepharmy.repo;
 
 import org.example.onlinepharmy.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
